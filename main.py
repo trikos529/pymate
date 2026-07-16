fa62587378a2ddb011d3ec75b357afbd6b1faeac
Panagiotis Vamvakopoulos <130182939+Panagiotis897@users.noreply.github.com>
2026-07-16 12:07:13 +0300

    Upload new files

--- main.py
+++ main.py
@@ -6,15 +6,27 @@ import json
 from pathlib import Path
 
 def main():
-   # Get the user's home directory 
+   config_add()
+
+def config_add(bg_color: str, text_color: str, btn_color: str, btn_atcive_color: str, btn_size: str, win_width: str, win_height: str, font: str, font_size: str, sidebar_bg: str, sidebar_width_percent: str):
+    # Get the user's home directory 
     home = Path.home()
 
+    # Read JSON data
     config_json = home /"AppData"/"Local"/"OpenActionReciever"/"config.json"
     if config_json.exists():
-        #read the file for config and call ui constructor
+        with config_json.open("r") as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                data = {}
+                home = None
         UI()
     else:  #default values
         UI()
+
+def default_values():
+    ...
 
 if __name__ == "__main__":
     main()
--- ui.py
+++ ui.py
@@ -1,26 +1,57 @@
 from tkinter import *
 
 class UI:
-    def __init__(self, bg_colour,text_colour,btn_colour,win_width,win_height,font):  #constructor pass json file path as argument to extract values 
-        self.win_sieze = win_width + "x" + win_height
+    def __init__(self, bg_colour, text_colour, btn_colour, 
+                 btn_size, btn_active_colour, win_width, win_height,
+                   font, font_size, sidebar_bg,
+                    sidebar_width_percent):
+        self.win_height = win_height
+        self.win_width = win_width
+        self.win_size = self.win_width + "x" + self.win_height
         self.text_colour = text_colour
         self.btn_colour = btn_colour
+        self.btn_size = btn_size
+        self.btn_active_colour = btn_active_colour
         self.bg_colour = bg_colour
-        self.font = font 
+        self.font = font
+        self.fontsize = font_size 
+        self.sidebar_bg = sidebar_bg
+        self.sidebar_width = (win_width / 100) * sidebar_width_percent
+        self.btn_width = (self.sidebar_width/100) * btn_size 
         self.win = Tk()
-        self.win.geometry(self.win_sieze) 
+        self.win.geometry(self.win_size) 
         self.win.title("Open Action Reciever")
-        self.win.resizable(width=False,height=FALSE)
+        self.win.resizable(width=False, height=FALSE)
         #self.logo = PhotoImage()               #logo to be made
         #self.win.iconphoto(True, logo)
         self.win.config(background=bg_colour)
-        UI.sidebar_draw()
-    
+        self.sidebar_draw()
+        self.actions_tab()
         self.win.mainloop()
         
-    def clear_content(self):    #helper caled froam any drawing func
+    def clear_content(self):    #helper called from any drawing func
         for widget in self.content_frame.winfo_children():
             widget.destroy()
-    def sidebar_draw(self):
-        
+    
+    def sidebar_draw(self):    
+        self.sidebar = Frame(self.win,bg=self.sidebar_bg, width=self.sidebar_width, height=self.win_height)
+        Button(self.sidebar,text="Actions", font=(self.font,self.fontsize),
+                relief=RAISED, active_background=self.btn_active_colour, command=self.actions_tab).pack
+        Button(self.sidebar,text="Intergrations", font=(self.font,self.fontsize),
+                relief=RAISED, active_background=self.btn_active_colour, command=self.intergrations_tab).pack
+        Button(self.sidebar,text="Connect", font=(self.font,self.fontsize),
+                relief=RAISED, active_background=self.btn_active_colour, command=self.connect_tab).pack
+        Button(self.sidebar,text="Settings", font=(self.font,self.fontsize),
+                relief=RAISED,active_background=self.btn_active_colour, command=self.settings_tab).pack
+
+    def actions_tab():
+        ...
+    
+    def intergrations_tab():
+        ...
+    
+    def connect_tab():
+        ...
+    
+    def settings_tab():
         ...
