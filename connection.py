5f89da563e8e1b1f3dff6d05a4e3d3dcf1caa6d4
thanasis theodwropoulos <trikos529@gmail.com>
2026-07-26 13:18:24 +0300

    Refactor Web-socket connection handling with alerts

--- connection.py
+++ connection.py
@@ -1,34 +1,34 @@
 import websockets
 import asyncio
 import json
+from ui import *
 
-port = 8764
 
-class Connect:
-    async def constructor(self):
+
+class Connection:
+    @staticmethod
+    async def establish(self,port):
         async with websockets.serve(self.handle_connection, "0.0.0.0", port):
-            print(f"WebSocket server running on ws://0.0.0.0:8764")
-            print("Press CTRL+C to stop the server")
+            ALERTS(UI.win,ALERTS.success_colour,f"WebSocket server running on ws://0.0.0.0:{port}",2000)
             await asyncio.Future()
-    
+    @staticmethod
     async def handle_connection(self, websocket):
         print("Device Linked!")
         try:
             async for message in websocket:
                 try:
                     data = json.loads(message)
-                    msg_type = data.get("type")
-                    state = data.get("state")
-                    btn_id = data.get("id")
-                
+                    # here we get recieved info and we will paass it on to the work quee
                 except json.JSONDecodeError as e:
-                    print(f"Error decoding JSON: {e}")
+                    ALERTS(UI.win,ALERTS.error_colour,f"Error decoding JSON: {e}",2000)
                     continue
                 except Exception as e:
-                    print(f"Error processing message: {e}")
+                    ALERTS(UI.win,ALERTS.error_colour,f"Error processing message: {e}",2000)
                     continue
             
         except websockets.exceptions.ConnectionClosed:
-            print("Device disconnected")
+            ALERTS(UI.win,ALERTS.warning_colour,"Device disconnected",2000)
+            
         except Exception as e:
-            print(f"Error in connection: {e}")
+            ALERTS(UI.win,ALERTS.error_colour,f"Error in connection: {e}",2000)
+            
